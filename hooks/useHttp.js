db41b92977c896f2ddeeaa60bccdbb2de0b692c1
Sertan Soydabas <ssoydabas41@gmail.com>
2022-09-27 08:50:23 +0300

    Custom React HTTP hook added

---
+++ hooks/useHttp.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function useHttp() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState(null);
+
+  const sendRequest = async (requestConfig, dataProcessingLogic) => {
+    setIsLoading(true);
+    setHttpError(null);
+
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        const errorInfo = await response.json();
+        throw new Error(errorInfo.errorMessage);
+      }
+
+      const data = await response.json();
+      dataProcessingLogic(data);
+    } catch (error) {
+      setIsLoading(false);
+      setHttpError(error.message);
+    }
+  };
+
+  return {
+    isLoading,
+    setIsLoading,
+    httpError,
+    setHttpError,
+    sendRequest,
+  };
+}
+
+export default useHttp;
