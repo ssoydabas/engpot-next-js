4549dc153a26523ef10f2b207b67e0a19bc3a048
Sertan Soydabas <ssoydabas41@gmail.com>
2022-09-27 08:43:35 +0300

    Setting up env files and their orientation

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    NEXT_ENV: process.env.NEXT_ENV,
+    API_URL: process.env.API_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
