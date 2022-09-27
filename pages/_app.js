d2af0aafc2a0a7819cd8ebc2cf5404fecc92579c
Sertan Soydabas <ssoydabas41@gmail.com>
2022-09-27 08:47:54 +0300

    React Redux Provider - Authentication Refresher - Layout implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import './styles/globals.css'
+import "./styles/globals.css";
+
+import store from "../store/store";
+import { Provider } from "react-redux";
+
+import Layout from "../components/ui/layout/Layout";
+import AuthenticationRefresher from "../util/components/authenticationRefresher/AuthenticationRefresher";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <AuthenticationRefresher>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthenticationRefresher>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
